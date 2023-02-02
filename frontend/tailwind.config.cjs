34531e4e456a03cbfc4ebff016e3275e4bece7d8
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2023-02-02 13:47:15 +0100

    Basic mobile layout done

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      tablet: "768px",
+      desktop: "1024px",
+    },
   },
   plugins: [require("daisyui")],
 };
