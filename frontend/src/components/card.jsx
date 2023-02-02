9f761ddaff4154639326295bcff2b3b5eeaf9a21
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2023-02-02 16:02:33 +0100

    Fix card overflow for 320px mobile

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -1,6 +1,6 @@
 export default () => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-80 bg-base-100 shadow-xl">
       <figure className="px-10 pt-10">
         <img
           src="https://picsum.photos/seed/picsum/400/200"
