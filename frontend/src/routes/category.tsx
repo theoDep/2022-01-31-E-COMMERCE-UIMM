0d5eafbabb465c354ec7bf14875e730e6a05aa11
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2023-02-08 16:22:59 +0100

    Create category route

---
+++ frontend/src/routes/category.tsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+import { useProductsCategory } from "../hooks/useProducts";
+import ProductList from "../pages/productList";
+
+export default () => {
+  const { category } = useParams();
+  const { products } = useProductsCategory(category);
+  return <ProductList products={products} category={category} />;
+};
